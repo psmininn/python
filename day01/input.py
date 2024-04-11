0ad94231c368008f619e6800552fed825b4d9716
psmininn <166698257+psmininn@users.noreply.github.com>
2024-04-11 17:13:16 +0900

    Add files via upload

---
+++ day01/input.py
@@ -0,0 +1,18 @@
+# print: 글 나오도록 하는 기능
+# input: 글을 입력하는 기능
+# drama = input("당신의 최애 드라마는?")
+# print(f"당신의 최애 드라마는 {drama}이군요!")
+
+# name = input("당신의 이름은?")
+# print = (f"당신의 이름은 {name}이군요!")
+
+#문제 1
+# name = input("당신의 이름은?")
+# age = input("당신의 나이는?")
+# color = input("당신이 좋아하는 색은?")
+# print(f"제 이름은 {name} , 나이는 {age} 살 이고, 좋아하는 색은 {color} 입니다!")
+
+#문제 2
+date = input("오늘의 날짜")
+plan = input("할 것")
+print(f"오늘의 날짜는 {date}, 할 계획은 {plan} 이군요!")
